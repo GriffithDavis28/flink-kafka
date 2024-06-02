aca617a02cd37bc360f94e36cefaa13fdc1fed29
griffith <griffithdavis1999@gmail.com>
2024-06-02 13:03:35 +0530

    kafka to file

--- src/main/java/flinkKafkaExample/FlinkKafkaExample.java
+++ src/main/java/flinkKafkaExample/FlinkKafkaExample.java
@@ -1,9 +1,8 @@
 package flinkKafkaExample;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.configuration.CheckpointingOptions;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
@@ -13,46 +12,47 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
-
-import javax.security.auth.login.AppConfigurationEntry;
 import java.util.Properties;
 
 public class FlinkKafkaExample {
 
-
-
     public static void main(String[] args) throws Exception {
-        String inputTopic = "flink_input";
-        String outputTopic = "flink_output";
-        String consumerGroup = "SPARTANS";
-        String address = "localhost:9092";
+        try{
+            String inputTopic = "flink_input";
+            String outputTopic = "flink_output";
+            String consumerGroup = "SPARTANS";
+            String address = "localhost:9092";
 
-        final StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        environment.enableCheckpointing(5000);
+            final StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+            environment.enableCheckpointing(5000);
 
-        environment.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+            environment.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
 
-        environment.setStateBackend(new FsStateBackend("file:///home/davisgriffith/Analytics/project/checkpoints"));
+            environment.setStateBackend(new FsStateBackend("file:///home/griffith/IdeaProjects/checkpoints"));
 
-        FlinkKafkaConsumer<String> consumer = createStringConsumer(inputTopic, address, consumerGroup);
+            FlinkKafkaConsumer<String> consumer = createStringConsumer(inputTopic, address, consumerGroup);
 
-        DataStream<String> data = environment.addSource(consumer);
+            DataStream<String> data = environment.addSource(consumer);
 
-        StreamingFileSink<String> sink = StreamingFileSink
-                .forRowFormat(new Path("file:///home/davisgriffith/Analytics/project/my-flink-project"), new SimpleStringEncoder())
-                .build();
+            DataStream<String> filteredData = data.filter(value-> true);
 
-        FlinkKafkaProducer<String> producer = createStringProducer(outputTopic, address);
+            StreamingFileSink<String> sink = StreamingFileSink
+                    .forRowFormat(new Path("file:///home/griffith/IdeaProjects/flink"), new SimpleStringEncoder())
+                    .build();
 
-        data.map(new Capitalizer())
-                .addSink(producer);
+            FlinkKafkaProducer<String> producer = createStringProducer(outputTopic, address);
 
-        data.addSink(sink);
+            data.addSink(producer);
 
-        data.writeAsText("///home/davisgriffith/Analytics/project/consumedData.txt", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+            data.addSink(sink);
 
-        environment.execute();
+            data.writeAsText("///home/griffith/IdeaProjects/flink/consumedData.txt", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+            filteredData.writeAsText("///home/griffith/IdeaProjects/flink/filteredData.txt", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+            environment.execute("example");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
 
     public static FlinkKafkaConsumer<String> createStringConsumer(String topic, String kafkaAddress, String kafkaGroup){
@@ -66,6 +66,21 @@ public class FlinkKafkaExample {
 
     public static FlinkKafkaProducer<String> createStringProducer(String topic, String kafkaAddress){
         return  new FlinkKafkaProducer<>(kafkaAddress, topic, new SimpleStringSchema());
+    }
+
+    public static String extractfield(String json){
+        try{
+            ObjectMapper obj = new ObjectMapper();
+            JsonNode node = obj.readTree(json);
+
+            if(node.has("name")){
+                return  node.get("name").asText();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 
 
